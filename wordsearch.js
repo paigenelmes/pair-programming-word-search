05d65770c975321a65f131712fbe8748aa92e272
Paige Nelmes <designer@paigenelmes.com>
2022-10-19 11:29:44 -0700

    Updating Word Search after debugging

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = function (letters, word) {
+  if (wordSearchHorizontal(letters, word) === false) {
+    const transposed = transpose(letters);
+    return wordSearchHorizontal(transposed, word);
+  }
+};
+  
+const wordSearchHorizontal = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+  
+  
+const transpose = function(matrix) {
+  let result = [];
+  
+  for (const i in matrix[0]) {
+    let column = [];
+    for (const x in matrix) {
+      column.push(matrix[x][i]);
     }
-}
+    result.push(column);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
